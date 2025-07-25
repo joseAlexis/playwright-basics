e85066dbd4df7ad5b6c0b936ea6659665f7a5e3c
Jose Bolanos <joseAlexis>
2025-07-25 19:42:32 +0200

    feat: Minor fixes on test files

--- tests/child-windows.spec.ts
+++ tests/child-windows.spec.ts
@@ -3,11 +3,13 @@ import test, { BrowserContext, expect, Page } from "@playwright/test";
 let context: BrowserContext;
 let page: Page;
 
+test.use({ baseURL: "https://the-internet.herokuapp.com" });
+
 test.beforeEach(async ({ browser }) => {
   context = await browser.newContext();
   page = await context.newPage();
 
-  await page.goto('/windows');
+  await page.goto("/windows");
 });
 
 test.afterEach(async () => {
@@ -15,10 +17,10 @@ test.afterEach(async () => {
   await context.close();
 });
 
-test('should open a new window', async () => {
-  const childPagePromise = context.waitForEvent('page');
+test("should open a new window", async () => {
+  const childPagePromise = context.waitForEvent("page");
 
-  await page.click('text=Click Here');
+  await page.click("text=Click Here");
 
   const childPage = await childPagePromise;
   await expect(childPage.getByText("New Window")).toBeVisible();
@@ -29,15 +31,15 @@ test('should open a new window', async () => {
 });
 
 test("should opne a new page using promise.all", async () => {
-  await page.click('text=Click Here');
+  await page.click("text=Click Here");
 
   // Promises have 3 states: pending, fulfilled, rejected
   // Promise.all will wait for all promises to be fulfilled
   // If any of the promises is rejected, the entire Promise.all will be rejected
   // Promise.all will return an array of the results of the promises
   const [childPage] = await Promise.all([
-    context.waitForEvent('page'),
-    page.click('text=Click Here')
+    context.waitForEvent("page"),
+    page.click("text=Click Here"),
   ]);
 
   await expect(childPage.getByText("New Window")).toBeVisible();
